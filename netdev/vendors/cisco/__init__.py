e2585ac24891cba172fc2056e9868e1d7c41ddc2
Name Redacted <info@malicious.group>
2020-03-24 11:26:36 -0400

    Added CiscoTCL

--- netdev/vendors/cisco/__init__.py
+++ netdev/vendors/cisco/__init__.py
@@ -1,6 +1,7 @@
 from .cisco_asa import CiscoASA
 from .cisco_ios import CiscoIOS
+from .cisco_tcl import CiscoTCL
 from .cisco_iosxr import CiscoIOSXR
 from .cisco_nxos import CiscoNXOS
 
-__all__ = ["CiscoIOS", "CiscoIOSXR", "CiscoASA", "CiscoNXOS"]
+__all__ = ["CiscoIOS", "CiscoTCL", "CiscoIOSXR", "CiscoASA", "CiscoNXOS"]
